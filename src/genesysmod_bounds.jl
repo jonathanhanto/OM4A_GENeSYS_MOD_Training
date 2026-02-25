21500ee7d5d05cf0f76df9f814b3a7606a82236e
isebastienstore <sabastiandanbaibe@esp.sn>
2026-02-25 15:00:19 +0000

    try to push

--- src/genesysmod_bounds.jl
+++ src/genesysmod_bounds.jl
@@ -177,7 +177,13 @@ function genesysmod_bounds(model,Sets,Params, Vars,Settings,Switch,Maps)
     #
     if Switch.switch_dispatch == NoDispatch()
         for r ∈ Sets.Region_full
-            for t ∈ intersect(Sets.Technology, vcat(Params.Tags.TagTechnologyToSubsets["Transformation"],Params.Tags.TagTechnologyToSubsets["PowerSupply"], Params.Tags.TagTechnologyToSubsets["Transport"],Params.Tags.TagTechnologyToSubsets["SectorCoupling"], Params.Tags.TagTechnologyToSubsets["StorageDummies"], Params.Tags.TagTechnologyToSubsets["CHP"]))
+            for t ∈ intersect(Sets.Technology, vcat(
+                Params.Tags.TagTechnologyToSubsets["Transformation"],
+                Params.Tags.TagTechnologyToSubsets["PowerSupply"], 
+                Params.Tags.TagTechnologyToSubsets["Transport"],
+                Params.Tags.TagTechnologyToSubsets["SectorCoupling"], 
+                Params.Tags.TagTechnologyToSubsets["StorageDummies"], 
+                Params.Tags.TagTechnologyToSubsets["CHP"]))
                 JuMP.fix(Vars.NewCapacity[Switch.StartYear,t,r],0; force=true)
             end
             for t ∈ intersect(Sets.Technology, vcat(Params.Tags.TagTechnologyToSubsets["Biomass"],["HLR_Gas_Boiler","HLI_Gas_Boiler","HHI_BF_BOF",
